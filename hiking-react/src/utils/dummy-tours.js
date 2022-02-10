7f350e899804a432fec759651a0edf9704e22e93
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-02-09 22:06:59 -0500

    added dummy tours and they works

---
+++ hiking-react/src/utils/dummy-tours.js
@@ -0,0 +1,28 @@
+export const dummyTours = [{
+        _id: '1',
+        name: 'Great Tour',
+        description: 'Great Arlington Walking some',
+        date: '02/09/2022',
+        trai_length: 1,
+        difficulty: 'EASY',
+        max_participants: '10'
+    },
+    {
+        _id: '2',
+        name: 'Virginia Tour',
+        description: 'Great Arlington Walking some',
+        date: '02/09/2022',
+        trai_length: 1,
+        difficulty: 'MEDIUM',
+        max_participants: '10'
+    },
+    {
+        _id: '3',
+        name: 'Arlington Tour',
+        description: 'Great Arlington Walking some',
+        date: '02/09/2022',
+        trai_length: 1,
+        difficulty: 'HARD',
+        max_participants: '10'
+    },
+];
