2e0283d273f74e7fea7b52cb752f8e49537ebb66
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-02-11 13:43:21 -0500

    register validation works

--- hiking-react/src/components/FormRegister.jsx
+++ hiking-react/src/components/FormRegister.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -11,6 +12,55 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 const FormRegister = () => {
 
   const theme = createTheme();
+
+  const preset = {
+    username: '',
+    password: '',
+    password2: ''
+  };
+
+  const [formState, setFormState] = useState(preset);
+  const handleChange = (e) => {
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
+
+  const validator = () => {
+    let test = true;
+
+    if (formState.username === ''){
+      test = false;
+    }
+    if (formState.password === ''){
+      test = false;
+    }
+    if (formState.password2 === ''){
+      test = false;
+    }
+    if (formState.password !== formState.password2){
+      test = false;
+    }
+
+    return test;
+  }
+
+  const handleClickSubmit = (e) => {
+    // ovo je handle za obican click event na taster submit
+    e.preventDefault(); // ovo sprecava da browser automatski submituje
+    if (validator(formState)) {
+      // ukoliko je prosla validacija
+      console.log('click submit...')
+      console.log(formState)
+    } else {
+      // ukoliko ne prodje validaciju
+      window.alert('Form validation Error :(')
+    }
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -36,39 +86,46 @@ const FormRegister = () => {
             noValidate sx={{ mt: 1 }}
           >
             <TextField
+              label="Username"
+              name="username"
+              value={formState.username}
+              onChange={handleChange}
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              id="username"
+              autoComplete="username"
               autoFocus
             />
             <TextField
+              label="Password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+              type="password"
               margin="normal"
               required
               fullWidth
-              name="password"
-              label="Password"
-              type="password"
               id="password"
               autoComplete="current-password"
             />
             <TextField
+              label="Confirm Password"
+              name="password2"
+              value={formState.password2}
+              onChange={handleChange}
+              type="password"
               margin="normal"
               required
               fullWidth
-              name="password2"
-              label="Confirm Password"
-              type="password"
               id="password2"
             />
             <Button
-              type="submit"
+              type="button"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              onClick={handleClickSubmit}
             >
               Register
             </Button>
