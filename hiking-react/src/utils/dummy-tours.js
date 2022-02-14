7cdb441eabbfd37d2f4b81613878cfb8644ec4e3
Mladen Makivic <makivic.mladen1203@gmail.com>
2022-02-14 15:05:22 -0500

    added some tour filtering

--- hiking-react/src/utils/dummy-tours.js
+++ hiking-react/src/utils/dummy-tours.js
@@ -3,7 +3,7 @@ export const dummyTours = [{
         name: 'Great Tour',
         description: 'Great Arlington Walking some',
         date: '02/09/2022',
-        trai_length: 1,
+        trail_length: 10,
         difficulty: 'EASY',
         max_participants: '10'
     },
@@ -12,7 +12,7 @@ export const dummyTours = [{
         name: 'Virginia Tour',
         description: 'Great Arlington Walking some',
         date: '02/09/2022',
-        trai_length: 1,
+        trail_length: 24,
         difficulty: 'MEDIUM',
         max_participants: '10'
     },
@@ -21,7 +21,7 @@ export const dummyTours = [{
         name: 'Arlington Tour',
         description: 'Great Arlington Walking some',
         date: '02/09/2022',
-        trai_length: 1,
+        trail_length: 31,
         difficulty: 'HARD',
         max_participants: '10'
     },
